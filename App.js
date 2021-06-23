f6b5c3275f3993d7f7b78998e58f84af8b8161ba
Dmytro Kozak <nikroshet@gmail.com>
2021-06-23 08:27:39 +0300

    added fonts

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [loaded] = useFonts({
+        OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+        OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text style={{ fontFamily: 'OpenSans' }}>Open up App.js to start working on your app!</Text>
+            <Text style={{ fontFamily: 'OpenSansBold' }}>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
